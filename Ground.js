9c47e679b6ed62f312df9e0680527e46ad6d3759
sumukhi0 <80339955+sumukhi0@users.noreply.github.com>
2021-06-15 11:16:50 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,20 @@
+class Ground{
+    constructor(x,y,w,h) {
+      var options = {
+          isStatic: true,
+          'restitution':0,
+          'friction':0,
+          'density':1
+      }
+      this.body = Bodies.rectangle(x,y,w,h,options);
+      this.w = w; 
+      this.h = h;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("white");
+    rect(pos.x, pos.y, this.w,this.h);
+    }
+  };
